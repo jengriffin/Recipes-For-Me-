aa4d86631c171950fa915846cb804eec5a8c8f91
Jackson McHugh <jackson.mchugh@gmail.com>
2022-08-12 14:09:48 -0400

    added a home page

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -1,10 +1,10 @@
-import Client from './api'
+// import Client from './api'
 
-export const RegisterUser = async (data) => {
-  try {
-    const res = await Client.post('/auth/register', data)
-    return res.data
-  } catch (error) {
-    throw error
-  }
-}
+// export const RegisterUser = async (data) => {
+//   try {
+//     const res = await Client.post('/auth/register', data)
+//     return res.data
+//   } catch (error) {
+//     throw error
+//   }
+// }
