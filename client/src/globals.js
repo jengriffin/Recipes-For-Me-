544de64107b2bb80aa923899ec946a60447438bc
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2022-08-15 09:25:23 -0500

    swap out local for deploy db

--- client/src/globals.js
+++ client/src/globals.js
@@ -1,6 +1,6 @@
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'postgresql-trapezoidal-18213'
 
-const DATABASE_URL = 'postgresql-trapezoidal-18213 '
+const DATABASE_URL = 'http://localhost:3000'
 module.exports = {
   BASE_URL,
   DATABASE_URL
