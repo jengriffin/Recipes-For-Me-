d96cca26cef9e0489fd3cb998cdfa3613b2ea40f
Seng Kit Mun <cellkit11@gmail.com>
2022-08-15 18:19:37 -0400

    tryout commit

--- src/pages/Recipeslist.js
+++ src/pages/Recipeslist.js
@@ -2,9 +2,7 @@ import { useNavigate, Link } from 'react-router-dom'
 import axios from 'axios'
 import { useState } from 'react'
 import { useEffect } from 'react'
-// import { BASE_URL } from '../globals'
-
-// const BASE_URL = 'https://recipes-for-me-api.herokuapp.com/'
+import { BASE_URL } from '../globals'
 
 const Recipes = (props) => {
   let navigate = useNavigate()
@@ -12,16 +10,14 @@ const Recipes = (props) => {
   const [recipe, setRecipe] = useState('')
 
   const showRecipes = (recipe) => {
-    console.log(recipe._id)
+    console.log(recipe.id)
     navigate()
   }
 
   useEffect(() => {
     const getRecipes = async () => {
       console.log('get recipes')
-      let res = await axios.get(
-        'https://recipes-for-me-api.herokuapp.com/api/recipes/all'
-      )
+      let res = await axios.get(`${BASE_URL}/api/recipes/all`)
       console.log(res.data)
       setRecipe(res.data)
     }
@@ -35,7 +31,7 @@ const Recipes = (props) => {
             <div
               className="recipe-card"
               onClick={() => showRecipes(recipe)}
-              key={recipe._id}
+              key={recipe.id}
             >
               <img
                 style={{ display: 'block' }}
