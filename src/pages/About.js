765e0bc6a3373bbfcfaa776ed04e7422bf1c596c
Jackson McHugh <jackson.mchugh@gmail.com>
2022-08-16 09:17:12 -0400

    imported nav and side nav

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,13 @@
 import { Link } from 'react-router-dom'
+import Nav from '../components/Nav'
+import SideNav from '../components/SideNav'
 const About = () => {
   return (
     <div className="about">
+      <div>
+        <Nav />
+        <SideNav />
+      </div>
       <h3></h3>
     </div>
   )
