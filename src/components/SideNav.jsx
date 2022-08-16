bbda027c06106a9f2e4e786df07ac2087cf5857b
jlangner87 <josh.langner@gmail.com>
2022-08-15 20:33:27 -0500

    scroll to top feature on side nav functioning

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -3,16 +3,21 @@ import '../App.css'
 import '../sideNav.css'
 
 const SideNav =()=>{
+
+    
     return(
     <div className='sidenav'>
+          <div className='sidenav_links'>
+        <NavLink className='tab' to='/feed'>All Recipes</NavLink>
+        </div>
         <div className='sidenav_links'>
         <NavLink className='tab' to='/'>My Recipes</NavLink>
         </div>
         <div className='sidenav_links'>
         <NavLink className='tab' to='/'>Add a Recipe</NavLink>
         </div>
-        <div className='sidenav_links'>
-        <NavLink className='tab' to='/'>Back to Top</NavLink>
+        <div className='sidenav_links' >
+        <a className='tab' href='#top' >Back to Top</a>
         </div>
     </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,10 +14,10 @@ const Home = () => {
         <SideNav />
       </div>
       <img className="food" src="https://gdurl.com/7eIn" alt="yummy pic" />
-      <img className="food" src="https://gdurl.com/7eIn" alt="yummy pic" />
-      <img className="food" src="https://gdurl.com/7eIn" alt="yummy pic" />
-      <img className="food" src="https://gdurl.com/7eIn" alt="yummy pic" />
-      <img className="food" src="https://gdurl.com/7eIn" alt="yummy pic" />
+      <img className="food" src="https://gdurl.com/QlAC" alt="yummy pic" />
+      <img className="food" src="https://gdurl.com/U5_G" alt="yummy pic" />
+      <img className="food" src="https://gdurl.com/f2hu" alt="yummy pic" />
+      <img className="food" src="https://gdurl.com/Vb6G" alt="yummy pic" />
     </div>
   )
 }
