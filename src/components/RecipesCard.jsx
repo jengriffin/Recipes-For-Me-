397782e33e3ed7596fc36e0b17618619f84c3b69
Seng Kit Mun <cellkit11@gmail.com>
2022-08-16 20:32:06 -0400

    FUll CRUD

--- src/components/RecipesCard.jsx
+++ src/components/RecipesCard.jsx
@@ -32,9 +32,25 @@ const RecipesCard = (props) => {
     setRecipe(selectedRecipe)
   }, [])
 
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    // do something with the data in the component state
+    let res = await axios.put(`${BASE_URL}/api/recipes/${id}`, formState)
+    console.log(res)
+    // clear the form
+    setFormState(initialState)
+    alert('You have successfully updated the recipe!')
+    navigate(`/recipes/${id}`)
+  }
+
   const deleteRecipe = async () => {
     // console.log(id)
     let res = await axios.delete(`${BASE_URL}/api/recipes/${id}`, formState)
+    alert('You have successfully deleted the recipe!')
     navigate('/feed')
   }
 
@@ -45,6 +61,63 @@ const RecipesCard = (props) => {
         <img className="recipe_card" src={recipe.image} alt="recipe image" />
         <h4 className="recipe_subs">{recipe.ingredients}</h4>
         <h4 className="recipe_subs">{recipe.directions}</h4>
+        <h4 className="recipe_subs">{recipe.category}</h4>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            onChange={handleChange}
+            value={formState.title}
+            placeholder={recipe.title}
+          />
+          <label htmlFor="image">Image:</label>
+          <input
+            type="text"
+            id="image"
+            onChange={handleChange}
+            value={formState.image}
+            placeholder={recipe.image}
+          />
+          <label htmlFor="ingredients">Ingredients:</label>
+          <input
+            type="text"
+            id="ingredients"
+            onChange={handleChange}
+            value={formState.ingredients}
+            placeholder={recipe.ingredients}
+          />
+          <label htmlFor="directions">Directions:</label>
+          <input
+            type="text"
+            id="directions"
+            onChange={handleChange}
+            value={formState.directions}
+            placeholder={recipe.directions}
+          />
+          <label htmlFor="category">Category:</label>
+          <select
+            id="category"
+            onChange={handleChange}
+            value={formState.category}
+          >
+            <option value="Diabetic Friendly">Diabetic Friendly</option>
+            <option value="Gluten Free">Gluten Free</option>
+            <option value="Halal">Halal</option>
+            <option value="Kosher">Kosher</option>
+            <option value="Lactose Free">Lactose Free</option>
+            <option value="Low Sodium">Low Sodium</option>
+            <option value="Vegan">Vegan</option>
+            <option value="Vegetarian">Vegetarian</option>
+          </select>
+
+          <button id="updateRecipeBtn" type="submit">
+            Update Recipe
+          </button>
+        </form>
+
+        <button onClick={deleteRecipe}>Delete</button>
       </span>
     </div>
   )
